eff7c39c6e97d552e485ed346f5f7e21bd5b79cf
Qamar Raza <qamar.raza@nucleusteq.com>
2026-09-22 15:35:47 +0530

    Set proper model and vector store paths

--- rag/retriever.py
+++ rag/retriever.py
@@ -4,7 +4,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 VECTOR_DB_PATH = os.path.join(PROJECT_ROOT,"data","chroma_index_final")
 
-embedding_model = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+embedding_model = HuggingFaceEmbeddings(model_name="BAAI/bge-large-en-v1.5")
 vector_db = Chroma(persist_directory=VECTOR_DB_PATH,embedding_function=embedding_model)
 
 def search_policy(query, policy_id=None, k=4):
